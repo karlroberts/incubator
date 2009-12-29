da655bc28f5758aebd7ffb7490213560a7148fec
Karl Roberts <karl.roberts@owtelse.com>
2009-12-29 22:12:48 +1100

    First add

---
+++ configurator/src/main/java/com/owtelse/config/Configurator.java
@@ -0,0 +1,18 @@
+package com.owtelse.config;
+
+import java.util.List;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Configurator {
+
+    private List<TemplateConfig> template;
+
+    public Configurator(List<TemplateConfig> template) {
+        this.template = template;
+    }
+
+}
